853da7c767f632e44cdc40b26347795d1c9fd0b7
Kaique Rijkaard <kaiquerijkaardcc@gmail.com>
2020-01-08 14:59:52 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://app-gerenciamento-vendas.herokuapp.com",
     bucketBaseUrl : "https://gerenciamento-vendas.s3-sa-east-1.amazonaws.com"
 }
